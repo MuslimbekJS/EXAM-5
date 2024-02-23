a0f86b8e93e953ce86df4edf85fd8e1c7a0098a3
“MuslimbekJS” <reactjs077@gmail.com>
2024-02-23 12:03:34 +0500

    qwerty

--- js/main.js
+++ js/main.js
@@ -2,12 +2,9 @@ const studentList = document.getElementById('student-list');
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 const search = document.getElementById('search');
 
-// Tugmani bosganda dark mode ni toggle qilish
-darkModeToggle.addEventListener('click', function () {
-    // Body elementini olish
-    const body = document.body;
 
-    // Dark mode ni toggle qilish
+darkModeToggle.addEventListener('click', function () {
+    const body = document.body;
     body.classList.toggle('dark-mode');
 });
 function displayCountries(countries) {
@@ -29,13 +26,12 @@ function displayCountries(countries) {
             `
         });
         studentList.innerHTML = str;
-        
+
 
     }
 
 }
 
-// const search = document.getElementById('search');
 
 let url = fetch("https://countries-restapi.vercel.app/all?page=1&limit=8");
 search.addEventListener('input', function (e) {
@@ -50,45 +46,14 @@ search.addEventListener('input', function (e) {
     displayStudents(newStudents);
 });
 
-// async function details(productId) {
-//     try {
-//         const res = await fetch("https://countries-restapi.vercel.app/all?page=1&limit=8");
-//         const data = await res.json();
-//         console.log(data.data);
-//         const product = data.data.find((pr) => pr.id === productId);
-//         localStorage.setItem('country', JSON.stringify(product));
-
-//     } catch {
-//         console.error("Error!", error);
-//     }
-// }
-
-// API dan ma'lumotlarni olish va localStorage ga saqlash
-
-
-// function details(name) {
-//     if (name[0] == data[0]) {
-//         let mydata = JSON.stringify(data.data);
-//         let str = JSON.parse(mydata)
-//         localStorage.setItem("country", JSON.stringify(str));
-//         console.log(localStorage.getItem("country"));
-//     }
-
-// }
-
 async function getCountries() {
     try {
-        // const res = await fetch("")
         const res = await fetch('https://countries-restapi.vercel.app/all?page=1&limit=8');
         const data = await res.json();
-        // let nameObject = data[0].name;
-        // console.log(nameObject);
         displayCountries(data.data);
         console.log(data);
 
-        // let mydata = JSON.stringify(data.data);
-        // let str = JSON.parse(mydata)
-        // localStorage.setItem("country", JSON.stringify(str))
+        
 
 
     } catch (error) {
@@ -100,36 +65,7 @@ async function getCountries() {
 
 
 }
-// JSON.stringify(displayCountries(data));
 getCountries();
 
 
 
-
-// Mamlakat ma'lumotlaridan card yaratish va sahifaga qo'shish
-// function createCountryCard(country, index) {
-//     const card = document.createElement('div');
-//     card.classList.add('card');
-//     card.innerHTML = `
-//                 <img src="${country.flags.svg}" alt="salom" width=100 height= 200>
-//                 <h2>${country.name.common}</h2>
-//                 <p>Population: ${country.population}</p>
-//                 <button class="saveBtn" data-country-index="${country}">Save</button>
-//             `;
-
-//     // "Save" tugmasini bosganda localStorage ga saqlash
-//     const saveBtn = card.querySelector('.saveBtn');
-//     saveBtn.addEventListener('click', function () {
-//         const countryIndex = saveBtn.getAttribute('data-country-index');
-//         const selectedCountry = country[countryIndex];
-//         console.log(selectedCountry);
-//         // localStorage.setItem(`country_${index + 1}`, JSON.stringify(selectedCountry))
-//         localStorage.setItem('selectedCountry', JSON.stringify(selectedCountry));
-//         alert('Country saved to localStorage.');
-//     });
-
-//     return card;
-// }
-
-// // Sahifani yuklash va cardlarni yaratish
-// fetchCountriesAndCreateCards();
